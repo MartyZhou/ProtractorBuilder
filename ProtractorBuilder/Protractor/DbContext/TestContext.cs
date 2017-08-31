e4a4b252429d3b41ce8423d45d9c5f7e550cb3ec
Marty Zhou <martyzhou@outlook.com>
2017-09-01 07:09:38 +0800

    use EF core

--- ProtractorBuilder/Protractor/DbContext/TestContext.cs
+++ ProtractorBuilder/Protractor/DbContext/TestContext.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProtractorBuilder.Protractor.DbContext
 {
-    public class TestContext
+    public class TestContext: DbContext
     {
         public TestContext()
         {
