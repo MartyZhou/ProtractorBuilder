b8c3524d2a5d2ae275c5a3dae98d9147dc603593
Marty Zhou <martyzhou@outlook.com>
2017-09-01 00:13:05 +0800

    test

--- ProtractorBuilder/Controllers/CaseController.cs
+++ ProtractorBuilder/Controllers/CaseController.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using ProtractorBuilder.Protractor.Common;
+
 namespace ProtractorBuilder.Controllers
 {
-    public class StepController
-    {
-        public StepController()
-        {
-        }
-    }
+	[Route("api/[controller]")]
+	public class CaseController : Controller
+	{
+		[HttpGet]
+		public string[] GetActions()
+		{
+			return Enum.GetNames(typeof(ActionSequence));
+		}
+
+		[HttpGet]
+		public string[] GetLocators()
+		{
+			return Enum.GetNames(typeof(Locator));
+		}
+	}
 }
