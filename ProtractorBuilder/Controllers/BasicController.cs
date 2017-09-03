18fb6007e70728bacabadc542e1acf163a3d15f2
Marty Zhou <martyzhou@outlook.com>
2017-09-03 22:59:29 +0800

    return basic value

--- ProtractorBuilder/Controllers/BasicController.cs
+++ ProtractorBuilder/Controllers/BasicController.cs
@@ -1,22 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using ProtractorBuilder.Protractor.Common;
 
 namespace ProtractorBuilder.Controllers
 {
     [Route("api/[controller]")]
-	public class BasicController : Controller
-	{
-		[HttpGet("action")]
-		public string[] GetActions()
-		{
-            return Enum.GetNames(typeof(ActionSequence));
-		}
+    public class BasicController : Controller
+    {
+        [HttpGet("action")]
+        public IEnumerable<object> GetActions()
+        {
+            return from ActionSequence a in Enum.GetValues(typeof(ActionSequence))
+                         select new { Id = (int)a, Name = a.ToString() };
+        }
 
-		[HttpGet("locator")]
-		public string[] GetLocators()
-		{
-			return Enum.GetNames(typeof(Locator));
-		}
-	}
+        [HttpGet("locator")]
+        public IEnumerable<object> GetLocators()
+        {
+			return from Locator r in Enum.GetValues(typeof(Locator))
+				   select new { Id = (int)r, Name = r.ToString() };
+        }
+    }
 }
